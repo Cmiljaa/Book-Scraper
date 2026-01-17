5ee384fff08a058119ffac1e92833a1998c06bd4
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2026-01-17 23:28:07 +0100

    Add get data function

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import pandas
 
+url = 'https://books.toscrape.com/index.html'
+
+def get_data(url):
+	response = requests.get(url)
+
+	if response.status_code != 200:
+		print(f"Failed to receive the data. Status code: {response.status_code}")
+		return []
+	
+	soup = BeautifulSoup(response.text)
+
+	return soup.find_all('article', class_="product_pod")
+
