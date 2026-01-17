c25158fc4c3033971acc0b4a01698c56f7442e25
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2026-01-17 23:40:27 +0100

    Add extract book data function

--- main.py
+++ main.py
@@ -16,3 +16,23 @@ def get_data(url):
 
 	return soup.find_all('article', class_="product_pod")
 
+def extract_book_data(book):
+	name_tag = book.find('h3')
+	title = name_tag.findChild().text if name_tag else 'N/A'
+
+	price_tag = book.find('p', class_='price_color')
+	price = price_tag.text if price_tag else 'N/A'
+
+	in_stock_tag = book.find('p', class_='instock availability')
+	in_stock = in_stock_tag.text.strip() if in_stock_tag else 'N/A'
+
+	img_tag = book.find('img', class_='thumbnail')
+	img_url = img_tag['src']
+	full_img_url = urljoin(url, img_url)
+
+	return {
+		"title": title,
+        "price": price,
+        "in_stock": in_stock,
+        "image_url": full_img_url
+	}
