da4c08b0d2cc6a5edd253e11a63fabe7700d9df3
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2026-01-23 23:48:20 +0100

    Add get data function

--- book_page_scraper.py
+++ book_page_scraper.py
@@ -1,2 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+def get_data(url):
+	
+	response = requests.get(url, headers=HEADERS, timeout=10)
+
+	if response.status_code != 200:
+		print(f"Failed to receive the data. Status code: {response.status_code}")
+		return []
+	
+	soup = BeautifulSoup(response.text, "html.parser")
+
+	return soup.find('article', class_="product_page")
